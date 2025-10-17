bf67a22188c6b24f4c4cdbac64de4e4bec788a0f
LambdaLuke87 <LambdaLuke87@users.noreply.github.com>
2025-10-17 16:41:10 -0300

    Fixed tripmine anim

--- cl_dll/ev_hldm.cpp
+++ cl_dll/ev_hldm.cpp
@@ -1547,7 +1547,7 @@ void EV_TripmineFire(event_args_t* args)
 
 	//Hit something solid
 	if (tr.fraction < 1.0)
-		gEngfuncs.pEventAPI->EV_WeaponAnimation(PHYSGUN_FIRE, 0);
+		gEngfuncs.pEventAPI->EV_WeaponAnimation(TRIPMINE_DRAW, 0);
 
 	gEngfuncs.pEventAPI->EV_PopPMStates();
 }
